b9fcb74d0f394f93b78296afa08acc91421278c1
lionel <lionel@Captain.local>
2021-07-31 16:55:35 +0800

    fix promise chain

--- Promises/src/mypromise_v0.js
+++ Promises/src/mypromise_v0.js
@@ -2,39 +2,60 @@ class MyPromise {
   constructor(fct) {
     this.state = "pending";
     this.result;
-    this.nextPromise ;
-    this.onFulfilled ;
-    
-    if (fct) {
-      // invoke inner function and pass resolve() and reject() as arrow function to capture the this.
-      fct(
-        (r) => {
-          this.resolve(r);
-        },
-        (e) => {
-          this.reject(e);
-        }
-      );
-    }
+    this.nextPromise;
+    this.onFulfilled;
+
+    if (!fct) throw new TypeError("undefined is not a function");
+
+    // invoke callback and pass resolve() and reject() as arrow function to capture the this.
+    fct(
+      (r) => {
+        this.resolve(r);
+      },
+      (e) => {
+        this.reject(e);
+      }
+    );
   }
 
   then(onFulfilled) {
-    this.nextPromise = new MyPromise();
-    this.onFulfilled = onFulfilled;
-
-    // check if promise is already resolved
-    if (this.state == "fulfilled") {
-      this._doResolve(this.result);
-    }
+    // define next promise callback 
+    const fct = (resolve, reject) => {
+      switch (this.state) {
+        case "pending": {
+          // if promise still pending create enclosure to resole next promise when invoking callback
+          this.onFulfilled = () => {
+            const res = onFulfilled(this.result);
+            resolve(res);
+          };
+          break;
+        }
+        case "fulfilled": {
+          // if current promise already fulfiled, invoke onFulFilled callback and resolve next promise.
+          const res = onFulfilled(this.result);
+          resolve(res);
+          break;
+        }
+        case "rejected": {
+          // not implemented yet
+          break;
+        }
+      }
+    };
 
     // chain promises ...
+    this.nextPromise = new MyPromise(fct);
     return this.nextPromise;
   }
 
   resolve(result) {
     this.result = result;
     this.state = "fulfilled";
-    this._doResolve(result);
+    // check .then() not called yet, end of promise chain
+    if (this.nextPromise == null) return;
+    
+    // invoke callback
+    this.onFulfilled(result);
   }
 
   reject(error) {
@@ -43,24 +64,11 @@ class MyPromise {
     // no implemented yet
   }
 
-  _doResolve(result) {
-    // check if end of promise chain or if .then not called yet
-    if (this.nextPromise == null) return;
-
-    if (this.onFulfilled) {
-      // invoke callback
-      var res = this.onFulfilled(result);
-      // resolve next promise asynchronously
-      setTimeout(() => this.nextPromise.resolve(res), 0);
-    }
-  }
-
   toString() {
-    var s = `MyPromise {<${this.state}>`;
+    const s = `MyPromise {<${this.state}>`;
     return this.state == "pending" ? `${s}}` : `${s}: '${this.result}'}`;
   }
 }
-
 
 // global variables to track end of the last promise
 var isLastPromiseFulfilled = false;
@@ -69,28 +77,28 @@ var isLastPromisePending = true;
 
 const p1 = new MyPromise((resolve, reject) => {
   //const p1 = new Promise((resolve, reject) => {
-  console.log("Start Promise inner function, will resolve in 5s");
+  console.log("Start Promise callback, will resolve in 5s");
   setTimeout(() => {
     resolve("Resolved");
   }, 5 * 1000); // P1 -> delay resolve by 5 seconds
 });
 
 // build a promise chain, each call to .then() return a new Promise.
-const p2 = p1.then((r) => (`P1:${r}`));   // Promise 2 -> will resolve with: P1:Resolve
-const p3 = p2.then((r) => (`P2:${r}`));   // Promise 3 -> will resolve with: P2:P1:Resolve
-const p4 = p3.then((r) => (`P3:${r}`));   // Promise 4 -> will resolve with: P3:P2:P1:Resolve
+const p2 = p1.then((r) => `P1:${r}`); // Promise 2 -> will resolve with: P1:Resolve
+const p3 = p2.then((r) => `P2:${r}`); // Promise 3 -> will resolve with: P2:P1:Resolve
+const p4 = p3.then((r) => `P3:${r}`); // Promise 4 -> will resolve with: P3:P2:P1:Resolve
 
 p4.then((r) => {
   // toggle global var to track end of last promise
   isLastPromiseFulfilled = true;
   isLastPromisePending = false;
-  console.log(`-> P4:${r}`);   // P4 will display: -> P4:P3:P2:P1:Resolve 
+  console.log(`-> P4:${r}`); // P4 will display: -> P4:P3:P2:P1:Resolve
 });
 
 //  Create an interval to display promise's status every second.
 var logCount = 1;
 console.log("Start logging");
-var interval = setInterval(() => {
+const interval = setInterval(() => {
   console.log(`${logCount++} ---------------------------`);
   console.log(`P1:${p1}, P2:${p2}, P3:${p3}, P4:${p4}`);
   logCount++;
